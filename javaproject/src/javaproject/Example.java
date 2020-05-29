b021519ab6afaa895756c77b0cbede77cd9e099f
randhir <randhir_kr_jha@rediffmail.com>
2020-05-30 01:50:36 +0530

    First commit
    uploading all the files

---
+++ javaproject/src/javaproject/Example.java
@@ -0,0 +1,15 @@
+package javaproject;
+
+public class Example 
+
+{
+   public static void main(String args[]) 
+   {
+	   int x=10;
+	   System.out.println(x++);
+	   System.out.println(++x);
+	   System.out.println(x--);
+	   System.out.println(--x);
+	 }
+   
+}
